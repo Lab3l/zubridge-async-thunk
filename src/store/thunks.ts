6ac155b01f14a39170c850426dc0ffc18a501e61
Lab3l <tuisku.salminen@gmail.com>
2025-05-10 23:24:24 +0300

    2.0.0-next.6 reproducible

--- src/store/thunks.ts
+++ src/store/thunks.ts
@@ -4,7 +4,7 @@ export const testThunk = () => async (getState, dispatch) => {
   console.info('Test thunk start', new Date().toISOString())
   console.log('General value', path(['general', 'value'], await getState()))
   await dispatch('general.setValue', 2)
-  console.info('First value should be set', new Date().toISOString(), '. Current general value', path(['general', 'value'], await getState()))
+  console.info('First value should be set to 2', new Date().toISOString(), '. Current general value', path(['general', 'value'], await getState()))
   await dispatch('general.setValue', 10)
-  console.info('Second value should be set', new Date().toISOString(), '. Current general value', path(['general', 'value'], await getState()))
+  console.info('Second value should be set to 10', new Date().toISOString(), '. Current general value', path(['general', 'value'], await getState()))
 }
