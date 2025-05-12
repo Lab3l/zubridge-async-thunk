53b22d6754c45d13f1f9390999103964dac06ca3
Sam Maister <goosewobbler@protonmail.com>
2025-05-12 16:08:03 +0100

    chore: add namespace for debug

--- src/store/thunks.ts
+++ src/store/thunks.ts
@@ -1,10 +1,23 @@
-import { path } from 'ramda'
+import { State } from '.'
 
-export const testThunk = () => async (getState, dispatch) => {
-  console.info('Test thunk start', new Date().toISOString())
-  console.log('General value', path(['general', 'value'], await getState()))
-  await dispatch('general.setValue', 2)
-  console.info('First value should be set to 2', new Date().toISOString(), '. Current general value', path(['general', 'value'], await getState()))
-  await dispatch('general.setValue', 10)
-  console.info('Second value should be set to 10', new Date().toISOString(), '. Current general value', path(['general', 'value'], await getState()))
-}
+export const testThunk =
+  () => async (getState: () => State, dispatch: (action: string, payload: number) => Promise<void>) => {
+    console.info('[Lab3l] - Test thunk start', new Date().toISOString())
+    console.log('[Lab3l] - General value', await getState())
+    await dispatch('general.setValue', 2)
+    let state = await getState()
+    console.info(
+      '[Lab3l] - First value should be set to 2',
+      new Date().toISOString(),
+      '. Current general value',
+      state.general
+    )
+    await dispatch('general.setValue', 10)
+    state = await getState()
+    console.info(
+      '[Lab3l] - Second value should be set to 10',
+      new Date().toISOString(),
+      '. Current general value',
+      state.general
+    )
+  }
