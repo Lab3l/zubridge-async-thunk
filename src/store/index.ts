8e5863dfb73047d410d22e1ffa7b057f9c038b7b
Sam Maister <goosewobbler@protonmail.com>
2025-05-12 16:07:08 +0100

    fix: use correct state type

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,12 @@
-import { handlers as generalHandlers } from './general.ts'
+import { handlers as generalHandlers } from './general.js'
 
 export const actionHandlers = (store: Store) => ({
   ...generalHandlers(store),
 })
 
-export type Subscribe = (
-  listener: (state: State, prevState: State) => void,
-) => () => void
+export type Subscribe = (listener: (state: State, prevState: State) => void) => () => void
 export type Handlers = Record<string, () => void>
-export type State = { counter: number }
+export type State = { general: { value: number } }
 export type Store = {
   getState: () => State
   getInitialState: () => State
